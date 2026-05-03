672e928d80686e5ef054f245bc9495b803380337
RAS-BAVANI <rasbavani4@gmail.com>
2026-05-03 13:42:37 +0530

    Refine input prompt for marks entry
    Updated prompt for user input to be more concise.

--- src/main/java/com/gradecalc/gradecalc/App.java
+++ src/main/java/com/gradecalc/gradecalc/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter marks for subjects: ");
+        System.out.print("Enter marks: ");
         int marks = sc.nextInt();
 
         
